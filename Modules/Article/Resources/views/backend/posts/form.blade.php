13ff242c88bfba132b4d3270765a2aa088313f7d
vickyorigin <vickywork55@gmail.com>
2023-08-01 11:11:58 +0530

    add

--- Modules/Article/Resources/views/backend/posts/form.blade.php
+++ Modules/Article/Resources/views/backend/posts/form.blade.php
@@ -34,7 +34,7 @@
             $required = "required";
             ?>
             {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required"]) }}
+            {{ html()->text($field_name)->value('Super Admin')->placeholder($field_placeholder)->class('form-control')->attributes(["$required",'readonly']) }}
         </div>
     </div>
 </div>
@@ -111,7 +111,7 @@
 </div>
 
 <div class="row">
-    <div class="col-4">
+    <div class="col-6">
         <div class="form-group">
             <?php
             $field_name = 'type';
@@ -183,7 +183,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-5">
+    <div class="col-6">
         <div class="form-group">
             <?php
             $field_name = 'meta_title';
@@ -195,7 +195,7 @@
             {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required"]) }}
         </div>
     </div>
-    <div class="col-5">
+    <div class="col-6">
         <div class="form-group">
             <?php
             $field_name = 'meta_keywords';
@@ -222,7 +222,7 @@
 
 </div>
 <div class="row">
-    <div class="col-12 col-sm-6">
+    <div class="col-12 col-sm-12">
         <div class="form-group">
             <?php
             $field_name = 'meta_description';
