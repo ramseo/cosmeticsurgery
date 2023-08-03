c155a26a80ad3df8ddac8eb3faeadadb53da5a38
vickyorigin <vickywork55@gmail.com>
2023-08-03 14:14:17 +0530

    ad

--- Modules/Cms/Resources/views/frontend/pages/popular-surgeries.blade.php
+++ Modules/Cms/Resources/views/frontend/pages/popular-surgeries.blade.php
@@ -62,8 +62,8 @@
                 </div>
 
                 <?php
-                $popular_surgeries1 = popular_cities_surgeries("popular-surgeries", $skip = 0, $take = 20);
-                $popular_surgeries2 = popular_cities_surgeries("popular-surgeries", $skip = 20, $take = 18);
+                $popular_surgeries1 = popular_cities_surgeries("popular-surgeries", $skip = 0, $take = 18);
+                $popular_surgeries2 = popular_cities_surgeries("popular-surgeries", $skip = 18, $take = 18);
                 ?>
 
                 <p class="identity text-center">Popular Surgeries</p>
