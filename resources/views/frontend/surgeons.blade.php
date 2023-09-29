3d1331386e5bebfebe52dbd8ffcf51595faeecca
vickyorigin <vickywork55@gmail.com>
2023-09-29 14:56:25 +0530

    asd

--- resources/views/frontend/surgeons.blade.php
+++ resources/views/frontend/surgeons.blade.php
@@ -23,32 +23,11 @@
         <div class="identity surgeon-filter">
             <select id="surgeon-filter">
                 <option value="">Sort Surgeons By Alphabet</option>
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-                <option value="d">D</option>
-                <option value="e">E</option>
-                <option value="f">F</option>
-                <option value="g">G</option>
-                <option value="h">H</option>
-                <option value="i">I</option>
-                <option value="j">J</option>
-                <option value="k">K</option>
-                <option value="l">L</option>
-                <option value="m">M</option>
-                <option value="n">N</option>
-                <option value="o">O</option>
-                <option value="p">P</option>
-                <option value="q">Q</option>
-                <option value="r">R</option>
-                <option value="s">S</option>
-                <option value="t">T</option>
-                <option value="u">U</option>
-                <option value="v">V</option>
-                <option value="w">W</option>
-                <option value="x">X</option>
-                <option value="y">Y</option>
-                <option value="z">Z</option>
+                <?php foreach (range('A', 'Z') as $elements) { ?>
+                    <option value="<?= strtolower($elements) ?>">
+                        <?= $elements ?>
+                    </option>
+                <?php } ?>
             </select>
 
             <div class="sort-btn">
