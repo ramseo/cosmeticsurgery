f264100969f8f103260454c2b75e5f65cbdad523
vickyorigin <vickywork55@gmail.com>
2023-11-02 13:09:41 +0530

    asdf

--- Modules/Cms/Resources/views/frontend/pages/surgery_cost.blade.php
+++ Modules/Cms/Resources/views/frontend/pages/surgery_cost.blade.php
@@ -103,6 +103,7 @@
         <!-- surgeons listing -->
         <?php
         $doctors = DB::table('users')->select('*')->whereNotNull('city')->Where('is_active', 1)->orderBy("sortable")->get()->toArray();
+        $alphabetical = DB::table('users')->select('*')->whereNotNull('city')->Where('is_active', 1)->orderBy("first_name")->get()->toArray();
         if ($doctors) {
         ?>
             <p class="identity">our surgeons:</p>
@@ -111,7 +112,7 @@
                     <option value="">Sort Surgeons By Alphabet</option>
                     <?php
                     $alphabets_arr = [];
-                    $fr_name_arr = array_column($doctors, "first_name");
+                    $fr_name_arr = array_column($alphabetical, "first_name");
                     foreach ($fr_name_arr as $item) {
                         if (!in_array($item[0], $alphabets_arr)) {
                             array_push($alphabets_arr, $item[0]);
